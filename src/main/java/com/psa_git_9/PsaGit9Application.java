122c08a7c972598030275b693b4c43c917e1eee8
yogacure <yogacure2345@gmail.com>
2025-12-11 12:05:28 +0530

    First Commit

---
+++ src/main/java/com/psa_git_9/PsaGit9Application.java
@@ -0,0 +1,25 @@
+package com.psa_git_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaGit9Application {
+
+	public static void main(String[] args) {
+        String name="mike";
+        int x=100;
+        boolean isPresent=false;
+        int y=200;
+        char v='a';
+        int b=200;
+        double z=10.3;
+        boolean k=false;
+        float m=10.3f;
+        char i='b';
+        char ch='c';
+        boolean isSub=true;
+        SpringApplication.run(PsaGit9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/psa_git_9/PsaGit9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psa_git_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaGit9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
